740a57b9c40b1c8e8ec7f763b95d343a9e67fa97
idrissjuanito <idrissjuanito@gmail.com>
2023-09-22 23:49:08 +0100

    task00: puppet manifests for creating a file on agent 2

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 
 node 'defaults' {
     file { '/tmp/school':
-        ensure  => 'present',
+        ensure  => present,
         owner   => 'www-data',
         group   => 'www-data',
         mode    => '0744',
