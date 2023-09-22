8e4c281e8ff6ad3dc510470bed7d40d62ef5cca3
idrissjuanito <idrissjuanito@gmail.com>
2023-09-22 22:48:26 +0100

    task00: puppet manifests for creating a file on agent

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+node 'defaults' {
+    file {'/tmp/school',
+        ensure => present,
+        owner => 'www-data',
+        group => 'www-data',
+        mode => '0744',
+        content => 'I love Puppet'
+    }
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,2 @@
+# ALX SE DEVOPS PROJECT
+## Configuration management and automation with puppet
