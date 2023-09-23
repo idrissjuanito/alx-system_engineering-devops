43c530ab65730889c32035aaf6bebc37d5cc75f9
idrissjuanito <idrissjuanito@gmail.com>
2023-09-23 02:33:42 +0100

    task02: running command from puppet manifest 3

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # puppet manifest to kill a process using exec
 Class default {
-    exec {'kill process':
-        command => ['/bin/pkill -9 killmenowa',
-        path    => ['/usr/bin'],
+    exec {'pkill -9 killmenow':
+        path => ['/usr/bin']
     }
 }
