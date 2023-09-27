7f0241da053559d091c9e18139546f3a3e37f50f
idrissjuanito <idrissjuanito@gmail.com>
2023-09-27 03:01:42 +0100

    install and configure nginx with puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,27 @@
+node 'default' {
+    package { 'nginx':
+        ensure  => installed,
+    }
+    service { 'nginx':
+        ensure  => running,
+        enable  => true,
+        require => Package['nginx'],
+    }
+    file { 'index_file':
+        ensure  => present,
+        path    => '/usr/share/nginx/html/index.html',
+        content => 'Hello World!',
+    }
+    file_line { 'port':
+        ensure  => present,
+        path    => '/etc/nginx/conf.d/default.conf',
+        line    => '    listen\t80;',
+        match   => 'listen *80;'
+    }
+    file_line { 'redirect':
+        ensure => present,
+        path   => '/etc/nginx/conf.d/default.conf',
+        line   => "    location /redirect_me {\n\treturn 301 https://projuanito.com;\n    }\n    location / {\n",
+        match  => 'location +/ {',
+    }
+}
