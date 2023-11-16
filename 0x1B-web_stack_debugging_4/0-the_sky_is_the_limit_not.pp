7151e421f3949b1661fe5ffb082025d18e9d813a
idrissjuanito <idrissjuanito@gmail.com>
2023-11-16 23:54:49 +0100

    repairing nginx server: High request fail rate

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+node default {
+    exec { 'edit files':
+      command => 'echo "fs.file-max = 70000" >> /etc/sysctl.conf;\
+        echo "www-data\tsoft\tnofile\t10000" >> /etc/security/limits.conf;\
+        echo "www-data\thard\tnofile\t30000" >> /etc/security/limits.conf;\
+        sysctl -p',
+      path    => ['/bin', '/sbin'],
+    }
+    exec { 'restart nginx':
+        command => 'nginx -s stop && nginx',
+        path    => ['/usr/sbin', '/usr/bin'],
+    }
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,2 @@
+# ALX SE Web debugging project
+# Project focuses on repairing and nginx server with huge requeest failures
