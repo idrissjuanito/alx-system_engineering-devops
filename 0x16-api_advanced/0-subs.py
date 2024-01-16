e03b5c1f7a30055fe93a4432b812fbe21bcf02cf
idrissjuanito <idrissjuanito@gmail.com>
2024-01-16 15:10:19 +0000

    api-advanced: query subreddit subscribers 5

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,8 @@ def number_of_subscribers(subreddit):
     """
         quries the reddit api based on the subreddit argument
     """
-    res = requests.get(subreddit, allow_redirects=False)
+    url = f"https://www.reddit.com/r/{subreddit}.json"
+    res = requests.get(url, allow_redirects=False)
     if res.status_code >= 400:
         return 0
     return res.json()["data"]["subscribers"]
